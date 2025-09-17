4199b49751f1da34206d9c60b2973797d414dbdc
Connor Belk <connor.belk@proton.me>
2025-09-17 02:23:55 -0400

    WIP: move changes to the dev branch

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <footer className="bg-slate-500">
+      <p className="text-center">&copy; 2025 Belk Web Development</p>
+    </footer>
+  );
+};
+
+export default Footer;
