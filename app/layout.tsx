b808e156aaaa555df9340aeeb835deb705ad4ab4
Connor Belk <connor.belk@proton.me>
2025-09-25 02:04:55 -0400

    fixed unused localFont import in main layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "./_components/Footer";
 import Navbar from "./_components/Navbar";
