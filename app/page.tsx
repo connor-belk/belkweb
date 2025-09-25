a1a18f6dcb588274389bf6197cd432ee6d397c98
Connor Belk <connor.belk@proton.me>
2025-09-25 02:03:06 -0400

    fixed horizonal scroll issue with font sizing.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 // import Image from "next/image";
+import { BiSolidTrafficCone } from "react-icons/bi";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center my-auto py-2">
-      <h1 className="text-6xl font-thin text-center">Belk Web Development</h1>
+    <div className="flex flex-col items-center justify-center my-auto mx-auto">
+      <BiSolidTrafficCone className="text-9xl text-center text-orange-500" />
+      <h1 className="text-5xl text-center">Belk Web Development</h1>
       <p>This site is under construction.</p>
     </div>
   );
