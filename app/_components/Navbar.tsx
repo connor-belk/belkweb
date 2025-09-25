9994b875cc755e718c78066f24e7613925664702
Connor Belk <connor.belk@proton.me>
2025-09-25 02:02:35 -0400

    fixed mobile navbar button to be stationary referencing viewport and not webpage and changed various visual features.

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { AiOutlineUp, AiOutlineClose } from "react-icons/ai";
+import { AiOutlineUp } from "react-icons/ai";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,14 +12,16 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="">
-      <Link href="/" className="text-3xl">
-        <h1>BWD</h1>
+    <nav className={`bg-gradient-to-b from-slate-500 to-slate-950/5`}>
+      <Link href="/" className="text-lg md:text-3xl">
+        <h1 className="text-center px-3 pt-3 pb-4">
+          &lt;h1&gt; Belk Web Development &lt;/h1&gt;
+        </h1>
       </Link>
 
       {/* Mobile Nav Button Bottom Right of Screen (Absolute Positioning) */}
 
-      <button className="absolute z-10 bottom-8 right-8 bg-slate-800 rounded-full border-2 border-slate-600 p-4">
+      <button className="fixed z-10 bottom-8 right-8 bg-slate-800 rounded-full border-2 border-slate-600 p-4">
         <AiOutlineUp
           className={`${
             isOpen ? "rotate-180 ease-in duration-300" : "ease-in duration-300"
@@ -31,9 +33,32 @@ const Navbar = () => {
       {/* Mobile Nav Menu Slide Up from Bottom of Screen (Absolute Positioning) */}
 
       <div
-        className={`fixed bottom-0 w-full h-[85%] bg-slate-800 transition-all duration-300 ease-in-out 
+        className={`fixed bottom-0 w-screen h-[85%] bg-slate-800 transition-all duration-300 ease-in-out 
     ${isOpen ? "translate-y-0" : "translate-y-full"}`}
-      ></div>
+      >
+        <ul className="h-full text-4xl flex flex-col items-center justify-center gap-6 p-4">
+          <li>
+            <Link href={"/"} onClick={handleClick}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href={"/services"} onClick={handleClick}>
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href={"/about"} onClick={handleClick}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href={"/contact"} onClick={handleClick}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
